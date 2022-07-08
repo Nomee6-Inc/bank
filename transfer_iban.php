f674516608d7747034d62e7f863470641a1c985d
Ali Yasin <46627616+aliyasiny65@users.noreply.github.com>
2022-07-08 12:17:48 +0300

    Create transfer_iban.php

---
+++ transfer_iban.php
@@ -0,0 +1,243 @@
+<?php
+session_start();
+include_once 'config.php';
+if (!isset($_SESSION['username'])) {
+    header("Location: index.php");
+} else {
+$result = mysqli_query($conn,"SELECT * FROM employee");
+$getusername = $_SESSION['username'];
+$getaccid = $_SESSION['accid'];
+$query = mysqli_query($conn,"SELECT * FROM users WHERE baccid = '$getaccid'");
+$result = $query->fetch_assoc();
+$money = $result['money'];
+$cardid = $result['cardid'];
+$dolar = $result['dolar'];
+
+
+if (isset($_POST['sendmoney'])) {
+$sendamount = $_POST['amount'];
+$sendiban = $_POST['iban'];
+
+if($money < $sendamount) {
+    echo "Bu işlemi gerçekleştirebilmek için yeterli miktarda paranız bulunmamaktadır.";
+} else {
+    $sql8 = "SELECT * FROM cards WHERE iban='$sendiban'";
+	$result8 = mysqli_query($conn, $sql8);
+	if ($result8->num_rows > 0) {
+	    $assoc8 = $result8->fetch_assoc();
+	    $cardnowmoney = $assoc8["money"];
+	    $cardnewmoney = $sendamount+$cardnowmoney;
+	    
+	    $sql5 = "UPDATE cards SET money = '$cardnewmoney' WHERE iban = '$sendiban'";
+        $run_query5 = mysqli_query($conn, $sql5);
+        
+	    $sendernewmoney = $money-$sendamount;
+	    
+	    $sql4 = "UPDATE users SET money = '$sendernewmoney' WHERE baccid = '$getaccid'";
+        $run_query4 = mysqli_query($conn, $sql4);
+	    if($run_query4 && $run_query5) {
+	        header("Refresh:2");
+	        echo "Para transfer işlemi başarılı!";
+	    } else {
+	        echo "Bir hata oluştu!";
+	    }
+	} else {
+	    echo "Belirttiğiniz IBAN'a sahip bir kullanıcı bulunamamaktadır.";
+	}
+}
+}
+}
+?>
+
+<!doctype html>
+<html lang="tr">
+  <head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <title>NOMEE6 BANK</title>
+    <!-- CSS files -->
+    <link href="https://devlet.nomee6.xyz/dist/css/tabler.min.css" rel="stylesheet"/>
+    <link href="https://devlet.nomee6.xyz/dist/css/tabler-flags.min.css" rel="stylesheet"/>
+    <link href="https://devlet.nomee6.xyz/dist/css/tabler-payments.min.css" rel="stylesheet"/>
+    <link href="https://devlet.nomee6.xyz/dist/css/tabler-vendors.min.css" rel="stylesheet"/>
+    <link href="https://devlet.nomee6.xyz/dist/css/demo.min.css" rel="stylesheet"/>
+    <meta property="og:title" content="NOMEE6 BANK" />
+    <meta property="og:url" content="https://bank.nomee6.xyz" />
+    <meta property="og:image" content="https://nomee6.xyz/assets/A.png" />
+    <meta property="og:description" content="E-Devlette ki eski klasik banka sistemi yeni modern ve gelişmiş bir sisteme taşındı." />
+	<?php 
+	$username = $_SESSION['username'];
+	echo("
+	<!-- Matomo -->
+	  <script>
+		var _paq = window._paq = window._paq || [];
+		_paq.push(['trackPageView']);
+		_paq.push(['enableLinkTracking']);
+		_paq.push(['setUserId', '$username']);
+		_paq.push(['enableHeartBeatTimer']);
+		(function() {
+			var u=\"https://matomo.aliyasin.org/\";
+		  _paq.push(['setTrackerUrl', u+'matomo.php']);
+		  _paq.push(['setSiteId', '16']);
+		  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+		  g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+		})();
+	  </script>
+	  <!-- End Matomo Code -->
+	");
+	?>
+  </head>
+  <body>
+    <div class="wrapper">
+      <header class="navbar navbar-expand-md navbar-light d-print-none">
+        <div class="container-xl">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-menu">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <h1 class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
+            <a href=".">
+              <img src="https://devlet.nomee6.xyz/static/logo.svg" width="150" height="35" alt="banka" class="navbar-brand-image">
+            </a>
+          </h1>
+          <div class="navbar-nav flex-row order-md-last">
+            <div class="nav-item d-none d-md-flex me-3">
+              <div class="btn-list">
+              </div>
+            </div>
+            <a href="?theme=dark" class="nav-link px-0 hide-theme-dark" title="Koyu Temayı Etkinleştir" data-bs-toggle="tooltip" data-bs-placement="bottom">
+              <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" /></svg>
+            </a>
+            <a href="?theme=light" class="nav-link px-0 hide-theme-light" title="Açık Temayı Etkinleştir" data-bs-toggle="tooltip" data-bs-placement="bottom">
+              <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><circle cx="12" cy="12" r="4" /><path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7" /></svg>
+            </a>
+            <div class="nav-item dropdown">
+              <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown" aria-label="Open user menu">
+                <div class="d-none d-xl-block ps-2">
+                  <div><?php echo($_SESSION['username']) ?></div>
+                </div>
+              </a>
+              <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+              <a href="logout.php" class="dropdown-item">Çıkış Yap</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <div class="navbar-expand-md">
+        <div class="collapse navbar-collapse" id="navbar-menu">
+          <div class="navbar navbar-light">
+            <div class="container-xl">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link" href="panel.php" >
+                    <span class="nav-link-icon d-md-none d-lg-inline-block">
+                      <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><polyline points="5 12 3 12 12 3 21 12 19 12" /><path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" /><path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" /></svg>
+                    </span>
+                    <span class="nav-link-title">
+                      Kartlarım
+                    </span>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="exchange.php" >
+                    <span class="nav-link-icon d-md-none d-lg-inline-block">
+                      <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-currency-dollar" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <path d="M16.7 8a3 3 0 0 0 -2.7 -2h-4a3 3 0 0 0 0 6h4a3 3 0 0 1 0 6h-4a3 3 0 0 1 -2.7 -2"></path>
+                        <path d="M12 3v3m0 12v3"></path>
+                      </svg>
+                    </span>
+                    <span class="nav-link-title">
+                      Döviz
+                    </span>
+                  </a>
+                </li>
+                <li class="nav-item active">
+                  <a class="nav-link" href="transfer_iban.php" >
+                    <span class="nav-link-icon d-md-none d-lg-inline-block">
+                      <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-transfer-in" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <path d="M4 18v3h16v-14l-8 -4l-8 4v3"></path>
+                        <path d="M4 14h9"></path>
+                        <path d="M10 11l3 3l-3 3"></path>
+                      </svg>
+                    </span>
+                    <span class="nav-link-title">
+                      IBAN Para Transferi
+                    </span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="page-wrapper">
+        <div class="container-xl">
+          <div class="page-header d-print-none">
+            <div class="row align-items-center">
+              <div class="col">
+                <h2 class="page-title">
+                  IBAN'a Para Transferi Yap
+                </h2>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="container-xl">
+        <form enctype="multipart/form-data" action="" method="POST">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="col">
+                            <div class="mb-3">
+                            <div class="form-label">Ödeme Yöntem</div>
+                            <select name="paymentmethod" class="form-select" required>
+                              <option value="wallet">Cüzdan</option>
+                            </select>
+                            </div>
+                            <div class="mb-3">
+                              <div class="form-label">Alıcı IBAN</div>
+                              <input name="iban" type="text" class="form-control" autocomplete="off" required/>
+                            </div>
+                            <div class="mb-3">
+                              <div class="form-label">Miktar</div>
+                              <input name="amount" type="number" class="form-control" required>
+                            </div>
+                            <div class="mt-2">
+                              <button name="sendmoney" class="btn btn-primary w-100">Para Gönder</button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+        </div>
+        <footer class="footer footer-transparent d-print-none">
+          <div class="container-xl">
+            <div class="row text-center align-items-center flex-row-reverse">
+              <div class="col-lg-auto ms-lg-auto">
+                <ul class="list-inline list-inline-dots mb-0">
+                  </li>
+                </ul>
+              </div>
+              <div class="col-12 col-lg-auto mt-3 mt-lg-0">
+                <ul class="list-inline list-inline-dots mb-0">
+                  <li class="list-inline-item">
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </div>
+    <!-- Libs JS -->
+    <script src="https://devlet.nomee6.xyz/dist/libs/apexcharts/dist/apexcharts.min.js"></script>
+    <!-- Tabler Core -->
+    <script src="https://devlet.nomee6.xyz/dist/js/tabler.min.js"></script>
+    <script src="https://devlet.nomee6.xyz/dist/js/demo.min.js"></script>
+  </body>
+</html>
