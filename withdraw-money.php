8fcce4510605d67ab11cb3bd9916b2dc1d6d49cc
Ali Yasin <46627616+aliyasiny65@users.noreply.github.com>
2022-10-23 12:47:38 +0300

    Security improvements
    - SQL sorguları için PDO kullanımı başlandı.
    - Birkaç hata giderildi.
    - Güvenlik açıkları düzeltildi.
    - Yeni session handler için sayfa uyumlu hale getirildi.

--- withdraw-money.php
+++ withdraw-money.php
@@ -1,24 +1,27 @@
 <?php
 session_start();
 include_once 'config.php';
+include 'api/SessionHandler.php';
 $getcardid = $_GET['cardid'];
-$getusername = $_SESSION['username'];
-if (!isset($_SESSION['username'])) {
-    header("Location: index.php");
-} else {
-$getaccid = $_SESSION['accid'];
-$query = mysqli_query($conn,"SELECT * FROM users WHERE baccid = '$getaccid'");
-$result = $query->fetch_assoc();
-$money = $result['money'];
-$cardid = $result['cardid'];
-$query2 = mysqli_query($conn,"SELECT * FROM cards WHERE cardid = '$getcardid'");
-$result2 = $query2->fetch_assoc(); 
-$cardmoney = $result2['money'];
-$cardowner = $result2['owner'];
+$getsessioncookie = $_COOKIE['sess_id'];
+if(sess_verify($getsessioncookie) == 1) {
+$get_user_unique_id = get_sess_user($getsessioncookie);
 
+$query = $db->query("SELECT * FROM users WHERE user_id = '{$get_user_unique_id}'",PDO::FETCH_ASSOC);
+$dataquery = $query->fetch(PDO::FETCH_ASSOC);
+$getusername = $dataquery['username'];
+$getuserrole = $dataquery['role'];
+$getusermoney = $dataquery['money'];
+$getuserdolar = $dataquery['dolar'];
+ 
+$card_query = $db->query("SELECT * FROM cards WHERE cardid = '{$getcardid}'",PDO::FETCH_ASSOC);
+$card__dataquery = $card_query->fetch(PDO::FETCH_ASSOC);
+$cardmoney = $card__dataquery['money'];
+$cardowner = $card__dataquery['owner'];
+  
 if($getusername != $cardowner) {
-    header("Location: panel.php");
-}
+    header("Location: panel");
+} else {
 
 if (isset($_POST['submit'])) {
 $cekilcekmiktar = $_POST['miktar'];
@@ -26,23 +29,39 @@ if($cardmoney < $cekilcekmiktar) {
     echo "Kartınızda bu kadar para bulunmuyor.";
 } else {
     if($getusername == $cardowner) {
-        $cekilcekmiktar = $_POST['miktar'];
         $newbankmiktar = $cardmoney-$cekilcekmiktar;
-        $newmoney = $money + $cekilcekmiktar - 1;
-        $sql7 = "UPDATE cards SET money = '$newbankmiktar' WHERE cardid = '$cardid'";
-        $run_query = mysqli_query($conn, $sql7);
-	    $sql8 = "UPDATE users SET money = '$newmoney' WHERE baccid = '$getaccid'";
-        $run_query8 = mysqli_query($conn, $sql8);
-        if($run_query && $run_query8){
-            echo "Başarıyla ₺$cekilcekmiktar tutarında para çektiniz.";
-            header("Location: panel.php");
+        $newmoney = $getusermoney + $cekilcekmiktar - 1;
+      	
+      	$update_card_money = $db->prepare("UPDATE cards SET
+			money = :new_money
+			WHERE cardid = :cardid");
+	 	$update_card_money_query = $update_card_money->execute(array(
+     		"new_money" => "$newbankmiktar",
+     		"cardid" => "$getcardid"
+		));
+      
+      	$update_user_money = $db->prepare("UPDATE users SET
+			money = :new_money
+			WHERE user_id = :userid");
+	 	$update_user_money_query = $update_user_money->execute(array(
+     		"new_money" => "$newmoney",
+     		"userid" => "$get_user_unique_id"
+		));
+        if($update_card_money_query && $update_user_money_query){
+            echo "<script>alert(\"Başarıyla ₺$cekilcekmiktar tutarında para çektiniz.\")</script>";
+            header("Location: panel");
         } else {
            echo "Bir hata oluştu!";
         }
     }
 }
 }
-};
+  
+}
+  
+} else {
+    header("Location: login");
+}
 ?>
 
 <!doctype html>
@@ -51,22 +70,21 @@ if($cardmoney < $cekilcekmiktar) {
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
     <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
-    <title>NOMEE6 BANK</title>
+    <title>Cüzdana Para Çek | Nomee6 Bank</title>
     <!-- CSS files -->
     <link href="https://devlet.nomee6.xyz/dist/css/tabler.min.css" rel="stylesheet"/>
     <link href="https://devlet.nomee6.xyz/dist/css/tabler-flags.min.css" rel="stylesheet"/>
     <link href="https://devlet.nomee6.xyz/dist/css/tabler-payments.min.css" rel="stylesheet"/>
     <link href="https://devlet.nomee6.xyz/dist/css/tabler-vendors.min.css" rel="stylesheet"/>
     <link href="https://devlet.nomee6.xyz/dist/css/demo.min.css" rel="stylesheet"/>
-	<?php 
-	$username = $_SESSION['username'];
+	<?php
 	echo("
 	<!-- Matomo -->
 	  <script>
 		var _paq = window._paq = window._paq || [];
 		_paq.push(['trackPageView']);
 		_paq.push(['enableLinkTracking']);
-		_paq.push(['setUserId', '$username']);
+		_paq.push(['setUserId', '$getusername']);
 		_paq.push(['enableHeartBeatTimer']);
 		(function() {
 			var u=\"https://matomo.aliyasin.org/\";
@@ -103,14 +121,24 @@ if($cardmoney < $cekilcekmiktar) {
             <a href="?theme=light" class="nav-link px-0 hide-theme-light" title="Açık Temayı Etkinleştir" data-bs-toggle="tooltip" data-bs-placement="bottom">
               <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><circle cx="12" cy="12" r="4" /><path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7" /></svg>
             </a>
+            <?php
+            if(strstr($_SERVER['HTTP_USER_AGENT'], "Nomee6 Bank Android Client")) { 
+	    		echo '
+             <div class="nav-item">
+              <a href="notifications" class="nav-link" tabindex="-1" aria-label="Bildirimleri Göster">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 5a2 2 0 0 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6" /><path d="M9 17v1a3 3 0 0 0 6 0v-1" /></svg>
+              </a>
+            </div>';
+    		};
+            ?>
             <div class="nav-item dropdown">
               <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown" aria-label="Open user menu">
                 <div class="d-none d-xl-block ps-2">
-                  <div><?php echo($_SESSION['username']) ?></div>
+                  <div><?php echo($getusername) ?></div>
                 </div>
               </a>
               <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-              <a href="logout.php" class="dropdown-item">Çıkış Yap</a>
+              <a href="logout" class="dropdown-item">Çıkış Yap</a>
               </div>
             </div>
           </div>
@@ -138,7 +166,7 @@ if($cardmoney < $cekilcekmiktar) {
                       <div class="ms-auto lh-1">
                       </div>
                     </div>
-                    <div class="h1 mb-3">₺<?php echo $money; ?></div>
+                    <div class="h1 mb-3">₺<?php echo $getusermoney; ?></div>
                   </div>
                 </div>
             </div>
@@ -185,31 +213,9 @@ if($cardmoney < $cekilcekmiktar) {
 				            Not: Her işlem için ₺1 işlem ücreti kesilmektedir.
 				        </small>
 					</div>
-				</div>
 				</form>
         </div>
     </div>
-        <footer class="footer footer-transparent d-print-none">
-          <div class="container-xl">
-            <div class="row text-center align-items-center flex-row-reverse">
-              <div class="col-lg-auto ms-lg-auto">
-                <ul class="list-inline list-inline-dots mb-0">
-                  </li>
-                </ul>
-              </div>
-              <div class="col-12 col-lg-auto mt-3 mt-lg-0">
-                <ul class="list-inline list-inline-dots mb-0">
-                  <li class="list-inline-item">
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </footer>
-      </div>
-    </div>
-    <!-- Libs JS -->
-    <script src="https://devlet.nomee6.xyz/dist/libs/apexcharts/dist/apexcharts.min.js"></script>
     <!-- Tabler Core -->
     <script src="https://devlet.nomee6.xyz/dist/js/tabler.min.js"></script>
     <script src="https://devlet.nomee6.xyz/dist/js/demo.min.js"></script>
